b64a8ba3bdbabbc0a777d165dfdd7ee52e904dec
Ashley Fogle <ms.ashleygreene@gmail.com>
2019-10-02 00:46:11 -0400

    added regex to validate email

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from flask import Flask, request, render_template
-
+import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-
+regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
 
 @app.route('/')
 def display_form():
-
     return render_template('index.html')
     
 
@@ -18,14 +17,14 @@ def validate_signup():
     username = request.form['username']
     password = request.form['password']
     verify = request.form['verify']
-    # email = request.signup_form['email']
+    email = request.form['email']
 
     # placeholders for errors that I will fill in later
     username_error = ''
     password_error = ''
     verify_error = ''
-    # email_error = ''
-    # blank_error = ''
+    email_error = ''
+
 
     if " " in username:
         username_error = "username can not include spaces"
@@ -36,7 +35,7 @@ def validate_signup():
     
     if " " in password:
         password_error = "Password can not include spaces"
-        password = ''
+        password = '' #need to clear out the password so that it does not show
     elif len(password) < 3 or len(password) > 20:
         password_error = "Must be 3 - 20 characters"    
         password = ''
@@ -45,12 +44,22 @@ def validate_signup():
         verify_error = "Passwords must match"
         password = ''
 
+    if not email == "":
+        if " " in email:
+            email_error = "Email can not include spaces"
+        elif len(email) < 3 or len(email) > 20:
+            email_error = "Must be 3 - 20 characters"
+        elif not (re.search(regex,email)): 
+            email_error = "Not a valid email"
+        
+
+
     #if there is nothing in errors, then that means no errors were found
-    if not username_error and not password_error and not verify_error:
+    if not username_error and not password_error and not verify_error and not email_error:
         return "Success!"
     else:
         return render_template('index.html', username=username, username_error=username_error, password_error=password_error,
-        verify_error=verify_error)
+        verify_error=verify_error, email=email, email_error=email_error)
     
 # @app.route('/welcome')
 # def valid_signup():
